b686ef8b997a19ca820b60a047261095b8c5af4a
zioguillo <pcisnerp@gmail.com>
2023-12-28 11:12:03 -0800

    Refactor Redis setup and add stream counter

--- app_api_streaming/app/app.py
+++ app_api_streaming/app/app.py
@@ -10,16 +10,16 @@ import aioredis
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Redis setup without decoding the responses
+# Redis setup
 REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', 'default_redis_password_if_not_set')
 redis = aioredis.from_url(
     "redis://127.0.0.1:6379",
     password=REDIS_PASSWORD
-    # Removed encoding and decode_responses
 )
 
-# Global frame id
+# Global frame id and stream counter
 frame_id = 0
+active_streams = 0
 
 async def store_frame_in_redis(frame_data):
     global frame_id
@@ -27,6 +27,13 @@ async def store_frame_in_redis(frame_data):
     await redis.set(key, frame_data)
     frame_id += 1
     return key
+
+async def clear_redis_cache():
+    keys = await redis.keys('frame:*')
+    for key in keys:
+        await redis.delete(key)
+    global frame_id
+    frame_id = 0
 
 @app.post("/upload")
 async def upload(file: UploadFile = File(...)):
@@ -40,11 +47,23 @@ async def upload(file: UploadFile = File(...)):
 
     return {"message": "Frame received"}
 
+@app.get("/video_feed")
+async def video_feed():
+    global active_streams
+    active_streams += 1
+
+    try:
+        return StreamingResponse(gen_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
+    finally:
+        active_streams -= 1
+        if active_streams == 0:
+            await clear_redis_cache()
+
 async def gen_frames():
     last_frame_id = -1
 
     while True:
-        await asyncio.sleep(0.01)  # Prevent high CPU usage
+        await asyncio.sleep(0.01)
         current_frame_id = frame_id - 1
         if current_frame_id != last_frame_id:
             frame_key = f"frame:{current_frame_id}"
@@ -53,10 +72,6 @@ async def gen_frames():
                 yield (b'--frame\r\n'
                        b'Content-Type: image/jpeg\r\n\r\n' + frame_data + b'\r\n')
                 last_frame_id = current_frame_id
-
-@app.get("/video_feed")
-async def video_feed():
-    return StreamingResponse(gen_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
 
 @app.get("/")
 async def index(request: Request):
